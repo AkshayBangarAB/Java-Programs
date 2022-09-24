3e0392b9ee43ec8e8d36499a351e0407077c94fd
Akshay Bangar <akshaybangar989@gmail.com>
2022-09-24 19:43:31 +0530

    Program to print multiplication table in java

--- src/Multiplication_Table.java
+++ src/Multiplication_Table.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class Multiplication_Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = sc.nextInt();
+        try {
+            if (n<=0){
+                throw new ArithmeticException("Enter a number greater than 0");
+            }
+            for (int i = 1; i<=10; i++){
+                int result = n * i;
+                System.out.printf("%d x %d = %d\n", n, i, result);
+            }
+        }
+        catch (Exception e) {
+        System.out.println(e);
+        }
+    }
 }
+
