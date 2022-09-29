cb90cc24d35cfb2ac125c3cbabe6f3979dc0302b
Akshay Bangar <akshaybangar989@gmail.com>
2022-09-29 11:54:28 +0530

    Program to print star pattern in java

---
+++ src/Star_Pattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Star_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n= sc.nextInt();
+        try {
+            if (n<=0){
+                throw new ArithmeticException("Enter number Greater than 0");
+            }
+            for (int i=1;i<=n;i++){
+                for (int j=1; j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
