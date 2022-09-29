0c804870b512ad496ee464e40090fddb3a920f17
Akshay Bangar <akshaybangar989@gmail.com>
2022-09-29 11:51:15 +0530

    Program to print star pattern in reverse order in java

--- src/Star_pattern_reverse.java
+++ src/Star_pattern_reverse.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-import java.util.SortedMap;
 
-public class Star_pattern_1 {
+public class Star_pattern_reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number:");
@@ -11,7 +10,7 @@ public class Star_pattern_1 {
                 throw new ArithmeticException("Enter number Greater than 0");
             }
             for (int i=1;i<=n;i++){
-                for (int j=1; j<=i;j++){
+                for (int j=n; j>=i;j--){
                     System.out.print("*");
                 }
                 System.out.println();
