58530903cfb154615384552773ec213cd5635e14
Akshay Bangar <akshaybangar989@gmail.com>
2023-01-26 22:05:32 +0530

    Program to print 1st n natural number in java

---
+++ src/Sum_of_first_n_even_number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sum_of_first_n_even_number {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i=0;i<=n;i++){
+            sum = sum+(2*i);
+        }
+        System.out.println("The sum of 1st n natural number is:"+sum);
+    }
+}
